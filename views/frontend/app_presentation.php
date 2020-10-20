e24177d4125799021e94493e8e037da8644993bb
Mathieu Jourdan <ma.jourdan@hotmail.fr>
2020-10-20 07:37:25 +0200

    edited titles architecture in both landing page and show page, unitied titles margins, unitied classes and links quotation mark, removed unused styles

--- views/frontend/app_presentation.php
+++ views/frontend/app_presentation.php
@@ -1,41 +1,44 @@
-<div class="main-content">
-        <div class="features">
-                <h1>Serial Binger<br><span class="subtitled">le mentor officiel de votre télé</span></h1>
+<div class='main-content'>
+        <div class='features'>
+                <div class='features-header'>
+                        <h1>Serial Binger</h1>
+                        <h2>le mentor officiel de votre télé</h2>
+                </div>
 
-                <section class="catalogue-card">
-                        <img src="./../../assets/images/vignets/compare.jpg" class="rounded" class="rounded" />
-                        <h2>Un catalogue pratique</h2>
+                <section class='app-intro'>
+                        <img src='./../../assets/images/vignets/display.jpg' class='rounded' />
+                        <h3>Un catalogue pratique</h3>
                         <p>Grâce à Serial Binger, vous avez accès à la liste intégrale des programmes de Netflix et Prime Video en un clin d'oeil.</p>
                 </section>
                 
-                <div class="splitter">
-                        <span class="splitter-underline"></span>
-                        <img src='./../../assets/images/icons/logo-serial-binger.png' class="transformed-logo" />
-                        <span class="splitter-underline"></span>
+                <div class='splitter'>
+                        <span class='splitter-underline'></span>
+                        <img src='./../../assets/images/icons/logo-serial-binger.png' class='transformed-logo' />
+                        <span class='splitter-underline'></span>
                 </div>
 
-                <section class="suggestions-card">
-                        <img src="./../../assets/images/vignets/advice.jpg" class="rounded" class="rounded" />
-                        <h2>Des suggestions sur mesure</h2>
-                        <p>Montrer-nous ce qui vous a plu ou non, afin que l'on vous propose continuellement des émissions qui vous correspondent.</p>
+                <section class='app-intro'>
+                        <img src='./../../assets/images/vignets/enlightement.jpg' class='rounded' />
+                        <h3>Des suggestions sur mesure</h3>
+                        <p>L'application vous propose des émissions qui vous correspond de plus en plus.</p>
                 </section>
                 
-                <div class="splitter">
-                        <span class="splitter-underline"></span>
-                        <img src='./../../assets/images/icons/logo-serial-binger.png' class="transformed-logo" />
-                        <span class="splitter-underline"></span>
+                <div class='splitter'>
+                        <span class='splitter-underline'></span>
+                        <img src='./../../assets/images/icons/logo-serial-binger.png' class='transformed-logo' />
+                        <span class='splitter-underline'></span>
                 </div>
 
-                <section class="binging-plan-card">
-                        <img src="./../../assets/images/vignets/plan.jpg" class="rounded" class="rounded" />
-                        <h2>Un suivi détaillé de votre binge-watching</h2>
+                <section class='app-intro'>
+                        <img src='./../../assets/images/vignets/follow-up.jpg' class='rounded' />
+                        <h3>Un suivi détaillé de votre binge-watching</h3>
                         <p>Vous savez à tout moment où vous en êtes de vos films et séries TV. Terminées, les impressions de déjà-vu !</p>
                 </section>
 
-                <div class="registration-box">
+                <div class='registration-box'>
                         <p>Accédez gratuitement à nos fonctionnalités</p>
-                        <a href="#"><i class="far fa-user-circle"></i>Créer un compte</a>
+                        <a href='#'><i class='far fa-user-circle'></i>Créer un compte</a>
                 </div>
 
-                <script src="./../../assets/js/random_layer_generator.js"></script>
+                <script src='./../../assets/js/random_layer_generator.js'></script>
         </div>
--- views/frontend/show_details.php
+++ views/frontend/show_details.php
@@ -1,71 +1,71 @@
-<div class="main-content">
-    <div class="show-block" id="<?php echo $_GET['type'] ?>">
+<div class='main-content'>
+    <div class='show-block' id='<?php echo $_GET['type'] ?>'>
     
         <h1></h1>
 
-        <section class="show-header">    
-            <p class="show-synopsis"></p>
+        <section class='show-header'>    
+            <p class='show-synopsis'></p>
         </section>
 
-        <section class="show-user-actions">
-            <div class="preshow-actions">
+        <section class='show-user-actions'>
+            <div class='preshow-actions'>
                 <!-- classe no-trailer ou available-trailer suivant le cas. Le style sera à ajouter ... -->
-                <button class="play-trailer-button">
-                    <i class="fas fa-play"></i><p>Bande-annonce</p>
+                <button class='play-trailer-button'>
+                    <i class='fas fa-play'></i><p>Bande-annonce</p>
                 </button>
 
-                <button class="watchlist-button">
-                    <i class="fas fa-list"></i>
+                <button class='watchlist-button'>
+                    <i class='fas fa-list'></i>
                     <p>Ajouter à ma liste</p>
                 </button>
             </div>
 
-            <div class="trailer-player">
+            <div class='trailer-player'>
             </div>
 
-            <div class="broadcaster-block">
+            <div class='broadcaster-block'>
                 <p>Voir sur :</p>
-                <button class="netflix-button">Netflix</button>
+                <button class='netflix-button'>Netflix</button>
             </div>
 
             <!-- sur mobile, ce bouton fera pop un formulaire au milieu de l'écran -->
-            <button class="show-overview-button" id="<?php echo $_GET['id']; ?>">Noter </button>
+            <button class='show-overview-button' id='<?php echo $_GET['id']; ?>'>Noter </button>
         </section>
 
-        <div class="splitter">
-            <span class="splitter-underline"></span>
-            <img src='./../../assets/images/icons/logo-serial-binger.png' class="transformed-logo" />
-            <span class="splitter-underline"></span>
+        <div class='splitter'>
+            <span class='splitter-underline'></span>
+            <img src='./../../assets/images/icons/logo-serial-binger.png' class='transformed-logo' />
+            <span class='splitter-underline'></span>
         </div>
 
-        <section class="show-info">
-            <h2>A propos :</h2>
+        <section class='show-info'>
+            <h3>A propos :</h3>
 
             <div>
-                <h3 class="show-genre-title">Genres :</h3>
-                <p class="show-genre-value">Non disponible</p>
+                <h4 class='show-genre-title'>Genres :</h4>
+                <p class='show-genre-value'>Non disponible</p>
             </div>
 
             <div>
-                <h3 class="show-length-title">Durée du film :</h3>
-                <p class="show-length-value">Non disponible</p>
+                <h4 class='show-length-title'>Durée du film :</h4>
+                <p class='show-length-value'>Non disponible</p>
             </div>
             
-            <div class="show-directors-container">
-                <h3 class="show-directors-title">Directeur :</h3>
-                <p class="show-directors-name">Non disponible</p>
+            <div class='show-directors-container'>
+                <h4 class='show-directors-title'>Directeur :</h4>
+                <p class='show-directors-name'>Non disponible</p>
+            </div>
+
+            <div class='show-casting'>
+                <h4>Distribution :</h4>
+                <ul class='casting-list'></ul>
             </div>
         </section>
 
-        <section class="show-casting">
-            <h3>Distribution :</h2>
-            <ul class="casting-list"></ul>
-        </section>
-
-        <script src="./../../assets/js/Slider.model.js"></script>
-        <script src="./../../assets/js/Show.model.js"></script>
-        <script src="./../../assets/js/Movie.model.js"></script>
-        <script src="./../../assets/js/TVShow.model.js"></script>
+        <script src='./../../assets/js/Slider.model.js'></script>
+        <script src='./../../assets/js/Show.model.js'></script>
+        <script src='./../../assets/js/Movie.model.js'></script>
+        <script src='./../../assets/js/TVShow.model.js'></script>
         
-        <script src="./../../assets/js/show_content_loader.js"></script>
+        <script src='./../../assets/js/show_content_loader.js'></script>
     </div>
